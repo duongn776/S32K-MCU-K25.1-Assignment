a48c9a916bf354bcd6b53219f5a88cb87a7a47dc
duongn776 <duongn776@gmail.com>
2025-09-28 21:38:54 +0700

    update .gitignore

--- Homework3/CMSIS_Driver_GPIO_PORT/Debug_FLASH/Drivers/Src/Driver_PORT.d
+++
@@ -1,7 +0,0 @@
-Drivers/Src/Driver_PORT.o: ../Drivers/Src/Driver_PORT.c \
- D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/Drivers/Inc/Driver_PORT.h \
- D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/include/S32K144.h \
- D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/Drivers/Inc/HWAccess_PORT.h
-D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/Drivers/Inc/Driver_PORT.h:
-D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/include/S32K144.h:
-D\:/Course_Online/MCU_S32K/assignment/Homework3/CMSIS_Driver_GPIO_PORT/Drivers/Inc/HWAccess_PORT.h:
